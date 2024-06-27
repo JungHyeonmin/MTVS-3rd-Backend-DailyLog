f95140a0cb094cacc59464cd2665b0b5a340818b
MungChi <2ee42a45@gmail.com>
2024-06-27 17:21:05 +0900

    6/27 Thur MySQL Join commit
    union 학습, 예제 풀기 완료

--- 02_MySQL/01_lecture-source/08_subquery.sql
+++ 02_MySQL/01_lecture-source/08_subquery.sql
@@ -18,3 +18,27 @@ select max(count)
 from (select count(*) as 'count'
       from (tbl_menu)
       group by category_code) as countmenu;
+
+-- union : 두 개 이상의 SELECT 문의 결과를 결합하여 중복된 레코드를 제거한 후 반환하는 SQL 연산자이다.
+
+SELECT
+    menu_code,
+    menu_name,
+    menu_price,
+    category_code,
+    orderable_status
+FROM
+    tbl_menu
+WHERE
+    category_code = 10
+UNION
+SELECT
+    menu_code,
+    menu_name,
+    menu_price,
+    category_code,
+    orderable_status
+FROM
+    tbl_menu
+WHERE
+    menu_price < 9000;
