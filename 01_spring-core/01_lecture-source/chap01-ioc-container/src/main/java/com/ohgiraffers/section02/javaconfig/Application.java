09d64e453ff0478fcc1827b4a3724b6676365279
MungChi <2ee42a45@gmail.com>
2024-06-29 18:56:16 +0900

    chap01-ioc-container 2회차 복습 중
    Java-based Configuration 복습
    - 주석 달기

--- 01_spring-core/01_lecture-source/chap01-ioc-container/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
+++ 01_spring-core/01_lecture-source/chap01-ioc-container/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
@@ -7,10 +7,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
     public static void main(String[] args) {
 
-        // ApplicationContext : 스프링 어플리케이션 전반에 걸쳐 모든 구성요소의 제어 작업을 담당하는 IOC 엔진 
-        ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class); // 설정파일의 클래스 메타정보를 입력해야 한다.
+        // ApplicationContext : 스프링 어플리케이션 전반에 걸쳐 모든 구성요소의 제어 작업을 담당하는 IOC 엔진
+        // AnnotationConfigApplicationContext() : 자바 스프링 컨테이너 중 하나이다. Java Configuration 형태의 Configuration Metadata 를 사용하여 Bean 을 생성한다.
 
-        MemberDTO member = context.getBean("member", MemberDTO.class); // 빈의 이름을 작성해야 한다.
+        // 생성자에 @Configuration 어노테이션이 달린 설정 클래스의 메타 정보를 전달한다.
+        // @Configuration 설정 파일의 위치를 지정한다.
+        ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
+
+        // @Configuration 설정 파일에서 "member"라는 이름을 가진 빈을 MemberDTO 클래스로 캐스팅하여 전달한다.
+        MemberDTO member = context.getBean("member", MemberDTO.class); // 빈의 id 를 작성해야 한다.
+
+        // bean 객체를 출력한다.
         System.out.println("member = " + member);
     }
 }
--- 01_spring-core/01_lecture-source/chap01-ioc-container/src/main/java/com/ohgiraffers/section02/javaconfig/ContextConfiguration.java
+++ 01_spring-core/01_lecture-source/chap01-ioc-container/src/main/java/com/ohgiraffers/section02/javaconfig/ContextConfiguration.java
@@ -6,16 +6,19 @@ import org.springframework.context.annotation.Configuration;
 
 // java 설정
 
-@Configuration // class 자체가 하나의 빈이 된다.
+// @Configuration : 해당 클래스가 Java 기반 Configuration Metadata 로 빈을 생성하는 클래스임을 표기한다.
+@Configuration
 public class ContextConfiguration {
 
-    // spring-context.xml과 같은 설정이다.
-    @Bean("member") // 빈의 이름을 설정
+    // @Bean : 해당 메서드의 반환값을 스프링 컨테이너에 빈으로 등록한다는 어노테이션
+
+    // 생성하는 bean 의 이름을 "member"로 표기한다. - bean 의 이름을 별도로 지정하지 않으면 메서드 이름을 bean 의 id 로 인식한다.
+    @Bean() // 빈의 이름을 설정
     public MemberDTO getMember() {
 
         System.out.println("getMember 호출함...");
 
+        // MemberDTO 의 객체를 빈으로 생성
         return new MemberDTO(1, "ueser01", "pass01", "홍길동");
     }
-
 }
