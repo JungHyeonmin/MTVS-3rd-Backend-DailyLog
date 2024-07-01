b7e51b7d1f942fd482f80a0e756428474735a0cf
MungChi <2ee42a45@gmail.com>
2024-07-01 22:51:43 +0900

    7/1 월 chap04-bean
    Properties 복습 1회차

--- 01_spring-core/01_lecture-source/chap04-bean/src/main/java/com/ohgiraffers/section03/properties/subsection01/properties/ContextConfiguration.java
+++ 01_spring-core/01_lecture-source/chap04-bean/src/main/java/com/ohgiraffers/section03/properties/subsection01/properties/ContextConfiguration.java
@@ -8,15 +8,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-// 값을 하드코딩하지 않고 properties로 변경, 재사용
+// 상품들을 빈으로 등록할 설정 파일을 작성한다. 이 때 product-info.properties 파일에 기재한 값으로 상품들의 값을 초기화 하려고 한다.
+// properties 파일을 읽어올 때 @PropertySource 어노테이션에 경로를 기재하여 읽어올 수 있으므로 읽어올 properties 파일의 경로를 작성한다.
 
+// Properties 는 키/값 쌍으로 이루어진 간단한 파일이다. 보통 소프트웨어 설정 정보를 저장할 때 사용된다.
+// 스프링에서는 Properties 를 이용하여 빈의 속성 값을 저장하고 읽어올 수 있다.
 @Configuration
-@PropertySource("section03/properties/subsection01/properties/product-info.properties") // 어떤 Properties 파일을 읽을건지 경로를 작성
+@PropertySource("section03/properties/subsection01/properties/product-info.properties") // 어떤 Properties 파일을 읽을건지 파일(경로)를 작성
 public class ContextConfiguration {
 
-    // 치환자(placeHolder) 문법을 이용하여 properties에 저장된 key를 입력하면 value에 해당하는 값을 꺼내온다.
-    // 없는 값을 경로로 지정하면 키값 그대로 추력한다. -> 해결방법 1. 키값 제대로 쓰기 2. 키값 대신 출력할 값을 작성
-    // 에러 방지를 위해서 대체 값을 적고 확인하는 것이 좋다.
+    /* 치환자(placeholder) 문법을 이용하여 properties 에 저장된 key 를 입력하면 value 에 해당하는 값을 꺼내온다.
+     * 공백을 사용하면 값을 읽어오지 못하니 주의한다.
+     * : 을 사용하면 값을 읽어오지 못하는 경우 사용할 대체 값을 작성할 수 있다. - 대체값을 작성하는 것을 권장한다.
+     **/
+
+    /*
+    필드 또는 파라미터에 @Value 어노테이션을 사용할 수 있으며
+    해당 어노테이션에 ${key} 와 같이 치환자(placeholder) 문법을 이용하여 properties 에 저장된 key 를 입력하면
+    value 에 해당하는 값을 꺼내와 필드 또는 파라미터에 주입한다.
+    key 뒤에 : 을 이용하여 해당 key 값이 없을 경우에는 주입할 기본 값을 입력할 수 있다.
+     */
+    // @Value("${키값:대체값}")
     @Value("${bread.carpbread.name2:슈크림붕어빵}") // 공백이 있으면 공백을 포함해서 경로를 찾기 때문에 공백이 있으면 안된다.
     private String carpBreadName;
 
--- 01_spring-core/01_lecture-source/chap04-bean/src/main/java/com/ohgiraffers/section03/properties/subsection02/i18n/ContextConfiguration.java
+++ 01_spring-core/01_lecture-source/chap04-bean/src/main/java/com/ohgiraffers/section03/properties/subsection02/i18n/ContextConfiguration.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+/**
+ * 국제화(Internationalization 또는 i18n)란, 소프트웨어를 다양한 언어와 문화권에 맞게 번역할 수 있도록 디자인하는 과정이다.
+ *
+ * Spring Framework 에서 i18n은 MessageSource 인터페이스와 property 파일을 이용하여 구현된다. 각 언어별로 property 파일을 정의하면,
+ * Spring 은 사용자의 로케일에 맞게 적절한 파일을 선택하여 애플리케이션 텍스트를 올바르게 번역할 수 있다.
+ */
 
 // 다국어 메세지 처리, 스프링이 이런것도 지원을 하는구나~ 정도로 이해
 @Configuration
 public class ContextConfiguration {
 
-    // 반드시 Bean으로 설정해야 해야 하는 메서드
+    // 반드시 Bean 으로 설정해야 해야 하는 메서드
     @Bean
     public ReloadableResourceBundleMessageSource messageSource() { // 메세지를 다시 받을 수 있는 저장소
 
