96cb254aae01c2176a67825acb644083eedf573a
MungChi <2ee42a45@gmail.com>
2024-07-02 12:45:02 +0900

    7/2 (화) JDBC 수업
    JDBC - MySQL driver 설치 후 Connection 객체 생성

--- 03_JDBC/01_lecture_source/chap01-connection/src/main/java/com/ohgiraffers/section01/connection/Application.java
+++ 03_JDBC/01_lecture_source/chap01-connection/src/main/java/com/ohgiraffers/section01/connection/Application.java
@@ -7,14 +7,20 @@ import java.sql.SQLException;
 public class Application {
     public static void main(String[] args) {
 
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/employee";
+        String user = "ohgiraffers";
+        String password = "ohgiraffers";
+
+        // null 로 초기화 한 이유 : scope 때문이다. try 블럭 내에서 선언 시에 finally 블럭에서 close()로 닫을 수 있도록 밖에서 선언과 초기화를 했다.
         Connection con = null;
 
         try {
             // 클래스 동적 로딩, 사용할 드라이버 등록
-            Class.forName("com.mysql.cj.jdbc.Driver"); // db를 바꾸면 ()안에만 바꾸면 된다.
+            Class.forName(driver); // db를 바꾸면 ()안에만 바꾸면 된다.
 
-            //  getConnection - add catch 옵션 선택
-                       con = DriverManager.getConnection("jdbc:mysql://localhost/employee", "ohgiraffers", "ohgiraffers");
+            // getConnection ("url", "user", "password") - add catch 옵션 선택
+            con = DriverManager.getConnection(url, user, password);
 
             System.out.println("con = " + con);
 
@@ -25,7 +31,7 @@ public class Application {
             throw new RuntimeException(e);
         } finally {
             if (con != null) {  // 통로를 열지 않고 닫으려고 하면 NullPointException 이 발생한다. 그래서 if 문으로 방지한다.
-                try { // NullPointException :
+                try { // NullPointException : 참조형(reference) 변수가 null 을 저장하고 null 값으로 이동하려고 할때 발생한다.
                     con.close();
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
