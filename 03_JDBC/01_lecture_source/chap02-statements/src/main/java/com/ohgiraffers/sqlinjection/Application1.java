e814fabfd134fb965b21d6aae40926718c16c58a
MungChi <2ee42a45@gmail.com>
2024-07-03 21:03:14 +0900

    7/2 (화) jdbc 복습
    jdbc - preparedStatement 복습 1회

--- 03_JDBC/01_lecture_source/chap02-statements/src/main/java/com/ohgiraffers/sqlinjection/Application1.java
+++ 03_JDBC/01_lecture_source/chap02-statements/src/main/java/com/ohgiraffers/sqlinjection/Application1.java
@@ -7,11 +7,10 @@ import java.sql.Statement;
 
 import static com.ohgiraffers.common.Template.close;
 import static com.ohgiraffers.common.Template.getConnection;
-import static com.ohgiraffers.common.Template.close;
-import static com.ohgiraffers.common.Template.getConnection;
 
 public class Application1 {
 
+    // 찾고자 하는 정보
     private static String empId = "200";
     private static String empName = "선동일";
 
@@ -19,17 +18,25 @@ public class Application1 {
     public static void main(String[] args) {
 
 
+        // db와 연결할 수 있는 객체 생성
         Connection con = getConnection();
+
+        // 요청문을 전달하고 받는 객체와 결과를 저장하는 객체는 null 로 초기화
         Statement stmt = null;
         ResultSet rset = null;
 
+        // query 문에 요청문 전달
         String query = "SELECT * FROM EMPLOYEE WHERE EMP_ID = '" + empId + "' AND EMP_NAME +'" + empName + "'";
-        System.out.println("query = " + query);
+        // System.out.println("query = " + query);
 
         try {
+            // createStatement() : getConnection 처럼 데이터베이스로 SQL 문을 보내기 위한 SQLServerStatement 개체를 만듭니다.
             stmt = con.createStatement();
+            // executeQuery(query) : 쿼리를 실행하고, 결과를 ResultSet 객체로 반환한다. 반환된 ResultSet 객체를 통해 결과를 가져올 수 있다.
             rset = stmt.executeQuery(query);
 
+            // next() : 커서의 다음 행이 존재할 경우 true 를 반환
+            // 커서(Cursor) : 일련의 데이터에 순차적으로 액세스할 때 검색 및 "현재 위치"를 포함하는 데이터 요소이다.
             if (rset.next()) {
                 System.out.println(rset.getString("EMP_NAME") + "님 환영합니다.");
             } else {
@@ -38,6 +45,7 @@ public class Application1 {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
+            // 자원(메모리에 할당된 인스턴스)이 아니라 운영체제의 자원(시스템 자원) 해제
             close(con);
             close(stmt);
             close(rset);
--- 03_JDBC/01_lecture_source/chap02-statements/src/main/java/com/ohgiraffers/sqlinjection/Application2.java
+++ 03_JDBC/01_lecture_source/chap02-statements/src/main/java/com/ohgiraffers/sqlinjection/Application2.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.sqlinjection;
+package com.ohgiraffers.section03.sqlinjection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,33 +11,43 @@ import static com.ohgiraffers.common.Template.getConnection;
 public class Application2 {
 
     private static String empId = "200";
-    private static String empName = "선동일";
+    private static String empName = "' OR 1=1 AND EMP_ID = '200";
 
     public static void main(String[] args) {
+
+        // db와 연결해주는 객체 생성, 요청문을 보내고 받는 객체와 데이터를 저장하는 객체는 null 로 초기화
         Connection con = getConnection();
         PreparedStatement pstmt = null;
         ResultSet rset = null;
 
-        String query = "SELECT * FROM EMPLOYEE WHERE EMP_ID = ? EMP_NAME = ?";
+        // EMP_ID 와 EMP_NAME 은 <?> 안에 아무 값이나 넣을 수 있도록 하고 넣은 값을 EMPLOYEE db 에서 조회하는 sql 문을 쿼리 문자열에 저장
+        String query = "SELECT * FROM EMPLOYEE WHERE EMP_ID = ? AND EMP_NAME = ?";
 
         try {
+            // query(요청문)문을 가지고 preparedStatement 객체 저장
             pstmt = con.prepareStatement(query);
+            
+            // ? 의 위치에 따라서 값을 전달
             pstmt.setString(1, empId);
             pstmt.setString(2, empName);
 
+            // executeQuery(query) : 쿼리를 실행하고, 결과를 ResultSet 객체로 반환한다. 반환된 ResultSet 객체를 통해 결과를 가져올 수 있다.
             rset = pstmt.executeQuery();
 
-            if (rset.next()) {
+            // 가져온 행에서 다음 커서가 있으면 
+            if(rset.next()) { // rset 에 저장된 값 중에서 EMP_NAME 컬럼을 문자열로 출력
                 System.out.println(rset.getString("EMP_NAME") + "님 환영합니다.");
             } else {
-                System.out.println("회원정보가 없습니다.");
+                System.out.println("회원 정보가 없습니다.");
             }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
-            close(con);
-            close(pstmt);
+            // 외부자원 닫기
             close(rset);
+            close(pstmt);
+            close(con);
         }
     }
 }
