41016c300455119181399c435c861e6a8512a4fa
MungChi <2ee42a45@gmail.com>
2024-07-10 22:15:04 +0900

    7/10 (수) Spring Boot 복습
    Method Mapping 1회차

--- 05_spring-webmvc/01_lecture-source/chap01-request-mapping/src/main/java/com/ohgiraffers/requestmapping/Chap01RequestMappingApplication.java
+++ 05_spring-webmvc/01_lecture-source/chap01-request-mapping/src/main/java/com/ohgiraffers/requestmapping/Chap01RequestMappingApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @Retention(RetentionPolicy.RUNTIME) : 런타임일때 실행
-// @SpringBootConfiguration
-//@EnableAutoConfiguration
-//@ComponentScan(
+/**
+ * 스프링 부트 애플리케이션 클래스는 @SpringBootApplication 어노테이션을 포함한다.
+ * 이 어노테이션은 @Configuration, @EnableAutoConfiguration, @ComponentScan 세 가지 어노테이션을 하나로 묶은 것이다.
+ * // @EnableAutoConfiguration 어노테이션은 Spring Boot 의 자동 설정을 활성화하고,
+ * // @ComponentScan 어노테이션은 스프링이 컴포넌트를 찾아서 빈으로 등록할 수 있도록 설정한다.
+ * // @Configuration 어노테이션을 사용하면, 하나 이상의 @Bean 이 붙은 메서드가 포함된 빈 설정용 클래스를 만들 수 있다.
+ */
 @SpringBootApplication // 인터페이스(런타임때 실행, 컴포넌트스캔이 활성되어있다.)
 public class Chap01RequestMappingApplication {
 
--- 05_spring-webmvc/01_lecture-source/chap01-request-mapping/src/main/java/com/ohgiraffers/requestmapping/ClassMappingTestController.java
+++ 05_spring-webmvc/01_lecture-source/chap01-request-mapping/src/main/java/com/ohgiraffers/requestmapping/ClassMappingTestController.java
@@ -9,21 +9,48 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 // 클래스와 메서드 매핑의 차이는? ㅜ머지..?
 
+/**
+ * ## 2-1. Method Mapping
+ *
+ * DispatcherServlet 이 @Controller 가 달린 컨트롤러 클래스에 웹 요청 처리를 위임하면
+ * //    컨트롤러 클래스의 handler Method 에 선언도니 다양한 @RequestMapping 설정 내용에 따른다.
+ *
+ * `@Controller` 어노테이션이 붙은 클래스는 웹 요청을 처리하는 컨트롤러임을 나타내며,
+ * Spring MVC 에서는 `@Controller` 어노테이션이 붙은 클래스를 자동으로 스캔해서 Bean 으로 등록한다.
+ * 이후 요청이 들어오면 `@RequestMapping` 어노테이션을 이용하여 어떤 메소드가 요청을 처리할지 지정한다.
+ */
 
+
+
+// @Controller : 스프링 컨테이너는 @Controller 어노테이션이 지정된 클래스를 빈으로 등록하고, 요청(request)을 해당 컨트롤러에 매핑하여 처리한다.
+
+// @RequestMapping : Spring Web MVC 에서 요청(Request)을 처리하는 데 사용되는 어노테이션
+// 상위 요청 order 에 대해서 모든 하위 요청을 선택
 @Controller
-@RequestMapping("/order/*") // 상위 요청 order 에 대해서 모든 하위 요청을 선택
+@RequestMapping("/order/*")
 public class ClassMappingTestController {
 
+    // @GetMapping : HTTP Get Method 에 해당하는 단축 표현으로 서버의 리소스를 조회할 때 사용
     // /order/regist
     @GetMapping("/regist")
     public String registOrder(Model model) {
 
+        // Model 객체는 Controller 에서 생성된 데이터를 담아 View 로 전달할 때 사용하는 객체이다.
+        // Model 은 스프링이 지원하는 기능으로써, key 와 value 로 이루어져있 HashMap 이다.
+
+        // add : 더하다 Attribute : 속성
+        // addAttribute(String name, Object value) : value 객체를 name 이름으로 추가한다. 뷰 코드에서는 name 으로 지정한 이름을 통해서 value 를 사용한다.
+        // 문자열 객체(GET ...)를 message 이름으로 추가한다.
         model.addAttribute("message", "GET 방식의 주문 등록용 핸들러 메서드 호출함");
 
         return "mappingResult";
     }
 
 
+    // @RequestMapping : Spring Web MVC 에서 요청(Request)을 처리하는 데 사용되는 어노테이션이며
+    // 이를 통해 어떤 URL 이 어떤 메소드에서 처리되는지, 어떤 HTTP Method 를 처리할지 등을 정할 수 있다.
+    
+    // RequestMethod : HTTP 요청 메서드를 열거한 Enum 클래스
     @RequestMapping(value = {"modify", "delete"}, method = RequestMethod.GET)
     public String modifyOrder(Model model) {
         model.addAttribute("message", "GET 방식의 주문 정보 수정과 주문 정보 삭제 공통 처리용 핸들러 메서드 호출함");
