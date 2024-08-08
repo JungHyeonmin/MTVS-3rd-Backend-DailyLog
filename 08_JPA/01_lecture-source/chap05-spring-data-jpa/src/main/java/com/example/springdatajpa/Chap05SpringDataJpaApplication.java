d60aabf2d5bb681e4736c9794f5a04854aa730af
MungChi <2ee42a45@gmail.com>
2024-08-08 13:11:59 +0900

    8/8 (목)  JPA 수업

---
+++ 08_JPA/01_lecture-source/chap05-spring-data-jpa/src/main/java/com/example/springdatajpa/Chap05SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap05SpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap05SpringDataJpaApplication.class, args);
+    }
+
+}
